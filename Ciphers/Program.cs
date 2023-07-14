fb0708a568e984ee847dc77755b09a8eef6039a8
gamesman36 <gamesman36@outlook.com>
2023-07-14 16:38:05 +0200

    Initial commit

---
+++ Ciphers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Ciphers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = "t h isi sames SAGE";
+            string result = StripAndLowerCase(input);
+            Console.WriteLine(result);
+            var transpositions = new List<(int n, int o)>();
+            transpositions.Add((1,3));
+            var transposer = new Transposer(result, transpositions);
+            Console.WriteLine(transposer.JumbleString());
+        }
+
+        static string StripAndLowerCase(string input)
+        {
+            // Regular expression to remove non-alphabetic characters
+            string stripped = Regex.Replace(input, "[^a-zA-Z]", "");
+
+            string lowerCase = stripped.ToLower();
+
+            return lowerCase;
+        }
+    }
+}
---
+++ Ciphers/Transposer.cs
@@ -0,0 +1,33 @@
+﻿namespace Ciphers
+{
+    internal class Transposer
+    {
+        private string _originalString;
+        private List<(int, int)> _transpositions;
+
+        public Transposer(string originalString, List<(int n, int o)> transpositions)
+        {
+            _originalString = originalString;
+            _transpositions = transpositions;
+        }
+
+        public string JumbleString()
+        {
+            char[] jumbledArray = _originalString.ToCharArray();
+
+            foreach (var (n, o) in _transpositions)
+            {
+                // Validity check
+                if (n >= 0 && n < _originalString.Length && o >= 0 && o < _originalString.Length)
+                {
+                    // Swapping characters at positions n and o
+                    char temp = jumbledArray[n];
+                    jumbledArray[n] = jumbledArray[o];
+                    jumbledArray[o] = temp;
+                }
+            }
+
+            return new string(jumbledArray);
+        }
+    }
+}
